24ac3c56839a42224bbb9b2949c4ced3a7fb23f7
ChunkAsgore <69470505+ChunkAsgore@users.noreply.github.com>
2020-08-16 09:23:27 -0500

    Add files via upload

---
+++ mcwl.py
@@ -0,0 +1,56 @@
+# -*- coding: utf-8 -*-
+import re
+import json
+
+usage = {
+    'tag': '-------------------- \u00A7bMCWL 帮助文档 \u00A7r--------------------',
+    'help': '''    添加玩家白名单 \u00A76> \u00A7r!!whitelist add [玩家名]
+    删除玩家白名单 \u00A76> \u00A7r!!whitelist remove [玩家名]
+    获取白名单列表 \u00A76> \u00A7r!!whitelist list
+    重新加载白名单 \u00A76> \u00A7r!!whitelist reload''',
+    'add': ' 用法 \u00A76> \u00A7r!!whitelist add [玩家名]',
+    'remove': ' 用法 \u00A76> \u00A7r!!whitelist remove [玩家名]'
+}
+display = {
+    'add': '已将{}加入白名单',
+    'remove': '已将{}移出白名单',
+    'list': '白名单列表：{}',
+    'reload': '已重新读取白名单'
+}
+
+def on_info(server, info):
+    if info.is_player == 1:
+        if info.content.startswith('!!whitelist') or info.content.startswith('!!wl'):
+            args = info.content.split()
+            if len(args) == 1:
+                server.say(usage['tag'])
+                for line in usage['help'].splitlines():
+                    server.say(line)
+                server.say(usage['tag'])
+            elif args[1] == 'add':
+                if len(args) == 2:
+                    server.say(usage['tag'])
+                    server.say(usage['add'])
+                    server.say(usage['tag'])
+                else:
+                    server.execute('whitelist add {}'.format(args[2]))
+                    server.say(display['add'].format(args[2]))
+            elif args[1] == 'remove':
+                if len(args) == 2:
+                    server.say(usage['tag'])
+                    server.say(usage['remove'])
+                    server.say(usage['tag'])
+                else:
+                    server.execute('whitelist remove {}'.format(args[2]))
+                    server.say(display['remove'].format(args[2]))
+            elif args[1] == 'list':
+                server.execute('whitelist list')
+                players = json.load(open('./server/whitelist.json'))
+                playernames = ''
+                for player in players:
+                    playernames = playernames + player['name'] + ', '
+                playernames = playernames[:-2]
+                server.say(display['list'].format(playernames))
+            elif args[1] == 'reload':
+                server.execute('whitelist reload')
+                server.say(display['reload'])
